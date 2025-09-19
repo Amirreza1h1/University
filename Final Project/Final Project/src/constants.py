e6de94cb2ba155041a8a13529ab39ae757f42caf
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2025-09-19 22:07:20 +0430

    final project

--- Final Project/Final Project/src/constants.py
+++
@@ -1,6 +0,0 @@
-RULES = {
-    ('Trusting', 'Trusting'): (-2, -2),
-    ('Trusting', 'Defiant'): (-2, 5),
-    ('Defiant', 'Trusting'): (5, -2),
-    ('Defiant', 'Defiant'): (-100, -100)  # dead!
-}
